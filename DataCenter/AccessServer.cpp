bb4b00446426745b151db3161296f98440790f92
me.xinxin <me.xinxin@gmail.com>
2012-10-22 14:15:25 +0000

    init

---
+++ DataCenter/AccessServer.cpp
@@ -0,0 +1,20 @@
+/*
+ * AccessServer.cpp
+ *
+ *  Created on: Oct 22, 2012
+ *      Author: mxx
+ */
+
+#include "AccessServer.h"
+
+AccessServer::AccessServer()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+AccessServer::~AccessServer()
+{
+	// TODO Auto-generated destructor stub
+}
+
---
+++ DataCenter/AccessServer.h
@@ -0,0 +1,18 @@
+/*
+ * AccessServer.h
+ *
+ *  Created on: Oct 22, 2012
+ *      Author: mxx
+ */
+
+#ifndef ACCESSSERVER_H_
+#define ACCESSSERVER_H_
+
+class AccessServer
+{
+public:
+	AccessServer();
+	virtual ~AccessServer();
+};
+
+#endif /* ACCESSSERVER_H_ */
